eaf1c581b0567f18c3bdc47a4c9c7e9bae464e18
Nikita <semikin91@gmail.com>
2023-03-02 15:40:54 +0200

    Added nuxt

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const test = ref('Page')
+</script>
+
+<template>
+  <section>
+    <h1>About {{ test }}</h1>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const test = ref('Welcome')
+</script>
+
+<template>
+  <div>
+    <h1>{{ test }} to the homepage</h1>
+  </div>
+</template>
