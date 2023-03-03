7967ccd6d588a28b9016bd8c6a22fa3552ef97ed
Nikita <semikin91@gmail.com>
2023-03-03 11:18:40 +0200

    Try nuxt pages

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('Nikita')
+</script>
+
+<template>
+  <h2>This is about page</h2>
+  <hr>
+  <NuxtLink to="/">
+    Back to Homepage
+  </NuxtLink>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('Nikita')
+</script>
+
+<template>
+  <h1>This is index page</h1>
+  <p>My name is {{ name }}</p>
+  <hr>
+  <NuxtLink to="/about">
+    About page
+  </NuxtLink>
+</template>
