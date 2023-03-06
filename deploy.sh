6e77fd337885837b26b10516d3939a528ab47b78
Nikita <semikin91@gmail.com>
2023-03-06 18:59:58 +0200

    changed base url

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:nightrunner91/listify.git master:gh-pages
+git push -f git@github.com:nightrunner91/listify.git main:gh-pages
 
 cd -
