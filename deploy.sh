76ecf97012bf837c3779d40a3642ae309853be50
Nikita <semikin91@gmail.com>
2023-03-06 18:48:50 +0200

    changed base url

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # abort on errors
-# set -e
+set -e
 
 # build
 npm run build
@@ -15,4 +15,4 @@ git commit -m 'deploy'
 
 git push -f git@github.com:nightrunner91/listify.git master:gh-pages
 
-# cd -
+cd -
