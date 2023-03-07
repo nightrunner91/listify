99eb0fce705020e843f766ec6794d2c044599778
Nikita <semikin91@gmail.com>
2023-03-07 17:00:20 +0200

    Removed custom button

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
-import LyButton from "@/components/ly-button/LyButton.vue";
 </script>
 
 <template>
-  <div class="d-flex gap-4 p-10">
-    <ly-button type="accented">accented</ly-button>
-    <ly-button type="filled">filled</ly-button>
-    <ly-button type="muted">muted</ly-button>
-    <ly-button type="outlined">outlined</ly-button>
-    <ly-button type="link">link</ly-button>
-  </div>
+  <div>About page</div>
 </template>
