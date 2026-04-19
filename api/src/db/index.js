16ec7887e2d29877fc46ea3a0ba21e3823afa6a4
Semikin Nikita <semikin91@gmail.com>
2026-04-19 22:20:08 +0300

    Update index.js

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -4,12 +4,21 @@ import * as schema from './schema.js'
 
 const { Pool } = pg
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+const poolConfig = {
   max: 10,
   idleTimeoutMillis: 30_000,
   connectionTimeoutMillis: 5_000,
-})
+}
+
+if (process.env.DATABASE_URL) {
+  poolConfig.connectionString = process.env.DATABASE_URL
+} else if (!process.env.PGHOST) {
+  console.error('[DB] CRITICAL ERROR: Neither DATABASE_URL nor PGHOST environment variables are found!')
+  console.error('[DB] If you are on Railway, make sure you linked the Postgres database to this service.')
+  process.exit(1)
+}
+
+const pool = new Pool(poolConfig)
 
 pool.on('error', (err) => {
   console.error('[DB] Unexpected pool error:', err)
