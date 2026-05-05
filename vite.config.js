a5cd14f682085d72a6d77b688e842c2f4bc505bb
Semikin Nikita <semikin91@gmail.com>
2026-05-05 18:31:29 +0300

    Add CNAME and set Vite base to root
    Add public/CNAME containing listify.js.org and update vite.config.js to use base '/' instead of '/listify/'. This aligns the app to be served from the root of the custom domain (listify.js.org) so asset paths and routing work correctly when deployed.

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/listify/',
+  base: '/',
   server: {port: 5173,},
   css: {preprocessorOptions: {scss: {additionalData: '@use "@/assets/styles/@core/globals.scss";'}}},
   resolve: {alias: {'@': fileURLToPath(new URL('./src', import.meta.url))}}
